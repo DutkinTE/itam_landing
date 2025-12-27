9957edf08825ef9e9145435a7d5f5c4de4c07de1
Timur Dutkin <timurdutkin@MacBook-Pro-Timur.local>
2025-12-27 14:10:27 +0300

    fix

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -168,7 +168,21 @@ export default function Achievements(): JSX.Element {
                     <div className="achievements__more">
                         <div className="achievements__moreRow">
                             <span className="achievements__moreLabel">Смотреть все</span>
-                            <span className="achievements__moreSquare" />
+                            <span className="achievements__moreSquare" >
+                                <svg
+                                    className="achievements__moreIcon"
+                                    width="22"
+                                    height="22"
+                                    viewBox="0 0 22 22"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        d="M14.5355 3.92909L13.1213 5.3433L17.7812 10.0031H1.39042V11.9972H17.7812L13.1213 16.657L14.5355 18.0712L21.6066 11.0002L14.5355 3.92909Z"
+                                    />
+
+                                </svg>
+                            </span>
                         </div>
                         <div className="achievements__moreLine" />
                     </div>
