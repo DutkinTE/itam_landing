f9215e91f956069ef327e1ae8ff44fa2f71700a4
Timur Dutkin <timurdutkin@MacBook-Pro-Timur.local>
2025-12-27 05:07:15 +0300

    edit appbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,34 @@
-import { type JSX } from "react";
+import { type JSX, useState } from "react";
 import "./header.css";
 
 export default function Header(): JSX.Element {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleToggleMenu = (): void => {
+        setIsActive((prev) => !prev);
+    };
+
     return (
-        <header className="header">
-            <div className="container header__inner">
-                <div className="brand">
-                    <img src="./images/Logo.svg" alt="Логотип IT at MISIS" className="brand__mark" />
+        <header className={`header`}>
+            <div className={`${isActive ? " header__active" : "header__disabled"}`}>
+                <div className={`container header__inner`}>
+                    <div className="brand">
+                        <img src="./images/Logo.svg" alt="Логотип IT at MISIS" className="brand__mark" />
+                    </div>
+                    <button
+                        type="button"
+                        className="header__menu"
+                        onClick={handleToggleMenu}
+                        aria-label="Открыть меню"
+                        aria-pressed={isActive}
+                    >
+                        <div className="header__menu1line"></div>
+                        <div className="header__menu2line"></div>
+                        <div className="header__menu3line"></div>
+                    </button>
                 </div>
-                <nav className="nav" aria-label="Навигация">
-                    <a className="nav__link" href="#highest-point">
-                        Главная
-                    </a>
-                    <a className="nav__link" href="#projects">
-                        Проекты
-                    </a>
-                    <a className="nav__link" href="#clubs">
-                        Клубы
-                    </a>
-                    <a className="nav__link" href="#team">
-                        Команда
-                    </a>
-                    <a className="nav__link" href="#blog">
-                        Блог
-                    </a>
-                </nav>
             </div>
+
         </header >
     );
 }
